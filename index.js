b34976460ea3ee4285e63bc71417bc702245763f
Ayda Mkaddem <92482497+AydaMkd@users.noreply.github.com>
2022-07-13 16:32:28 -0500

    commit

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
 
+
 //connect database
 connectDB();
 
+app.get("/", (req,res)=> res.send("API running"));
 
+//Routes
+
+app.use('api/users', require('./routes/api/users'));
 
 const port = process.env.PORT || 5000 ; 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
-app.get("/", (req,res)=> res.send("API running"));
+
