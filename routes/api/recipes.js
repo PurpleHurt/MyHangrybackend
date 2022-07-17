7d23383aea7d0ad6f6e703593ea37a06c69fcbe6
Ayda Mkaddem <92482497+AydaMkd@users.noreply.github.com>
2022-07-17 15:09:59 -0500

    recipe post

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -12,13 +12,13 @@ const checkObjectId = require('../../middleware/checkObjectId');
 
 router.post(
     '/',
-    [auth,[
+     auth,
     check('recipename', 'Recipe name is required').notEmpty(),
     check('cuisines', 'Cuisine is required').notEmpty(),
-    check('ingredients', 'Recipe ingredients are required').notEmpty(),
-    check('steps', 'Steps are required').notEmpty(),
-    check('directions', 'Directions are required').notEmpty(),
-    check('description', 'Description is required').notEmpty()]],
+    // check('ingredients', 'Recipe ingredients are required').notEmpty(),
+    // check('steps', 'Steps are required').notEmpty(),
+    //check('directions', 'Directions are required').notEmpty(),
+    check('description', 'Description is required').notEmpty(),
     async (req, res) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
@@ -28,19 +28,19 @@ router.post(
       try {
         const user = await User.findById(req.user.id).select('-password');
   
-        const newRecipe = new Recipe({
+        const  recipe = new Recipe({
           recipename: req.body.recipename,
           cuisines: req.body.cuisines,
-          ingredients: req.body.ingredients,
-          steps: req.body.steps,
-          directions: req.body.directions,
+        //   ingredients: req.body.ingredients,
+          // steps: req.body.steps,
+          // directions: req.body.directions,
           description: req.body.description,
-          name: user.name,
+          name: user.username,
           avatar: user.avatar,
           user: req.user.id
         });
   
-        const recipe = await newRecipe.save();
+         await recipe.save();
   
         res.json(recipe);
       } catch (err) {
@@ -50,5 +50,15 @@ router.post(
     }
   );
 
+  router.get('/', auth, async (req, res) => {
+    try {
+      const recipes = await Recipe.find().sort({ date: -1 });
+      res.json(recipes);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+ 
 
 module.exports = router;
