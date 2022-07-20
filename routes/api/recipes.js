2a65aff150fd2385a3b17bdcc969ae4b0ec37d53
Ayda Mkaddem <92482497+AydaMkd@users.noreply.github.com>
2022-07-20 13:55:12 -0500

    added comment route

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -112,5 +112,66 @@ router.post(
     }
   });
   
+  // post a comment
+router.post(
+  '/comment/:id',
+   auth,
+ 
+  
+  check('text', 'Text is required').notEmpty(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const recipe = await Recipe.findById(req.params.id);
+
+      const  newComment = {
+        text: req.body.text,
+        name: user.username,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+       recipe.comments.unshift(newComment)
+       await recipe.save();
+
+      res.json(recipe.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+//delete comment
+router.delete(
+  '/comment/:id/:comment_id',
+   auth, async (req, res) => {
+   
+    try {
+      
+      const recipe = await Recipe.findById(req.params.id);
+      // get the comment from the recipe
+      const comment = recipe.comments.find(comment=> comment.id == req.params.comment_id);
+      if (!comment){
+        return res.status(404).json({msg: 'Comment does not exist'});
+      }
+      // check user 
+      if (comment.user.toString()!== req.user.id){
+        return res.status(401).json({msg: "User not authorized"});
+      }
+    // remove index
+    const removeIndex = recipe.comments.map(comment=> comment.user.toString())
+    .indexOf(req.user.id);
+    recipe.comments.splice(removeIndex,1);
+    await recipe.save();
+    res.json(recipe.comments)
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 module.exports = router;
